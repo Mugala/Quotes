a7832cbb4b64789146198d8f74b094bf0ee9f5bd
mugala <mugalalevi21@gmail.com>
2018-03-19 17:16:00 +0300

    NEW APP QUOTES This application will gather quotes from various individual as well an up and down votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  quotes = [
+    new Quote(1, 'The sun is a star.','A.B Clark'),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith'),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons'),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos'),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons'),
+];
+  constructor(){  }
 }
