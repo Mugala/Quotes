e0751a5bcd359009ef24b36b639f20a0918b2181
mugala <mugalalevi21@gmail.com>
2018-03-23 11:23:23 +0300

    Styling This commit will add an image background to the page to improve on its appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Quote } from './quote'
 })
 export class AppComponent {
   quotes = [
-    new Quote(1, 'The sun is a star.','A.B Clark'),
-    new Quote(2, 'The moon is beautiful.', 'B.C Smith'),
-    new Quote(3, 'The earth is meh.', 'D.E Jacobsons'),
-    new Quote(4, 'The galaxy is large.', 'F.G Randos'),
-    new Quote(5, 'The universe is expanding.', 'I.J Persons'),
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
 ];
   constructor(){  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,17 +9,17 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
  
   quotes = [
-    new Quote(1, 'The sun is a star.','A.B Clark'),
-    new Quote(2, 'The moon is beautiful.', 'B.C Smith'),
-    new Quote(3, 'The earth is meh.', 'D.E Jacobsons'),
-    new Quote(4, 'The galaxy is large.', 'F.G Randos'),
-    new Quote(5, 'The universe is expanding.', 'I.J Persons'),
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
 ];
 
 addNewQuote(quote){
   let quoteLength = this.quotes.length;
   quote.id=quoteLength+1;
-  this.quotes.push(quote)
+  this.quotes.unshift(quote)
 
 }
 
